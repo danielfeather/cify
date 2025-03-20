64db6ab50da6cab41c4580a41213fabb44a9cfb6
Daniel Feather <12731372+danielfeather@users.noreply.github.com>
2025-03-20 00:24:41 +0000

    checking record length in test

--- tests/integration.rs
+++ tests/integration.rs
@@ -5,12 +5,16 @@ use std::io::Error;
 use cify::extract::Record;
 
 #[test]
-fn deserialize_array() -> Result<(), Error> {
+fn deserialize_array() -> Result<(), Box<dyn std::error::Error>> {
     let raw = fs::read_to_string("tests/extract.cif")?;
 
-    let result = cify::from_str::<Record>(&raw);
+    let result = cify::from_str::<Record>(&raw)?;
 
-    println!("Result: {:?}", result);
+    let Record::HD(data) = result else {
+        return Err(Box::new(cify::error::Error::Eof));
+    };
+
+    println!("{}", data.len());
 
     Ok(())
 }
