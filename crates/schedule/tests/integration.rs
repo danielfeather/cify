10e063392a06b64267919e6f5ae8b46d8297f8e3
Daniel Feather <12731372+danielfeather@users.noreply.github.com>
2025-03-31 13:08:04 +0100

    Moved to workspace

--- crates/schedule/tests/integration.rs
+++ crates/schedule/tests/integration.rs
@@ -1,12 +1,12 @@
 use std::fs;
 
-use cify::extract::Record;
+use schedule::extract::Record;
 
 #[test]
 fn deserialize_array() -> Result<(), Box<dyn std::error::Error>> {
     let raw = fs::read_to_string("tests/extract.cif")?;
 
-    let result = cify::from_str::<Vec<Record>>(&raw)?;
+    let result = schedule::from_str::<Vec<Record>>(&raw)?;
 
     println!(
         "{:#?}",
